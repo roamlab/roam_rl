929047c42ffb99240b4a8add37fdd97db5f85809
Gagan Khandate <gagank@cs.columbia.edu>
2019-11-06 15:37:39 -0500

    cleanup of GoalEnv interface

--- roam_rl/env/goal_env.py
+++ roam_rl/env/goal_env.py
@@ -17,21 +17,3 @@ class GoalEnv(gym.GoalEnv):
 
     def __init__(self, config_data, section_name):
         pass
-
-    def step(self, action):
-        super().step(action)
-
-    def reset(self):
-        super().reset()
-
-    def render(self, mode='human'):
-        super().render(mode=mode)
-
-    def close(self):
-        super().close()
-
-    def seed(self, seed=None):
-        super().seed(seed=seed)
-
-    def compute_reward(self, achieved_goal, desired_goal, info):
-        super().compute_reward(achieved_goal=achieved_goal, desired_goal=desired_goal, info=info)
